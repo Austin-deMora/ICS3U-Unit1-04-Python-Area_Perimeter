108253cb76682484ef78dc58dc47a68830277e74
Your Name <you@example.com>
2021-04-23 17:49:35 +0000

    Initial commit

---
+++ area_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Austin de Mora
+# Created on: April 2021
+# This program calculates the area and perimeter of a rectangle
+#    with the dimensions 5cm x 3cm
+
+
+def main():
+    # this function does area and perimeter
+    
+    print("If a rectangle has the dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm².".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+
+if __name__ == "__main__":
+    main()
